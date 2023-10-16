6e8c023a44b53b0ec2aff223a9c720c5a7966eea
Sahay Software <sahay.mtech2013@gmail.com>
2023-10-16 14:33:31 +0530

    User Entity Created with all fields

--- expense-management-api/src/main/java/com/amit/sahay/ems/entity/User.java
+++ expense-management-api/src/main/java/com/amit/sahay/ems/entity/User.java
@@ -1,6 +1,13 @@
 package com.amit.sahay.ems.entity;
 
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,7 +15,18 @@ import lombok.Data;
 @Data
 public class User {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
-	private String userName;
-	private String userPassword;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false,unique = true)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	private int age;
+	@CreationTimestamp
+	@Column(name = "created_at",updatable = false)
+	private Timestamp createdAt;
+	@Column(name="last-updated-at")
+	private Timestamp updatedAt;
 }
